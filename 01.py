2c734abe7d1f42542aa07a5cdddbd0e6288d9da7
Aljona13 <127626718+Aljona13@users.noreply.github.com>
2023-03-11 21:35:40 +0200

    dz_06.2

---
+++ 01.py
@@ -0,0 +1,10 @@
+chess_players = {
+"Carlsen, Magnus": 1865,
+"Firouzja, Alireza": 2804,
+"Ding, Liren": 2799,
+"Caruana, Fabiano": 1792,
+"Nepomniachtchi, Ian": 2773
+}
+
+new_chess_players = {x: y for y, x in chess_players.items()}
+print(new_chess_players)
